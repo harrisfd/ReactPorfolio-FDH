ff02e8614f3532dd4d2b978da4036391af3a4e8b
ldylicia <fharrisfoster@gmail.com>
2021-06-25 21:06:24 -0700

    updating links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,8 @@ function Footer() {
                         <h1>contact me</h1>
                         <p>Felicia Harris-Foster
                             <br />
-                            href={"mailto:fharrisfoster@gmail.com"}Email: fharrisfoster@gmail.com
-
+                            href={"mailto:fharrisfoster@gmail.com"}Email: fharrisfoster@gmail.com 
                         </p>
-
                         <p>Phone: 123-456-7890</p>
                         <br />
 
